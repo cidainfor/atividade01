094a337b2064825d301f6d1a6b0f76b1ed355b51
cleide <113257383+cidainfor@users.noreply.github.com>
2022-09-17 21:27:36 -0300

    Add files via upload

---
+++ questao01.py
@@ -0,0 +1,7 @@
+pe = int (input(f"Digite a medida em pés: "))
+polegada = pe*12
+jarda = pe/3
+milha = jarda/1760
+print(f"sua medida em polegadas é: {polegada}")
+print(f"sua medida em jardas é: {round(jarda,2)}")
+print(f"sua medida em milhas é: {round(milha,2)}")
