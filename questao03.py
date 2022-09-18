f10e1533bbf79da1a9abc7cd0a88209ea0d185cc
cleide <113257383+cidainfor@users.noreply.github.com>
2022-09-17 21:37:16 -0300

    Add files via upload

---
+++ questao03.py
@@ -0,0 +1,4 @@
+degrau = float(input(f"Digite a altura do degrau em metros: "))
+altura = float(input(f"Digite a altura que você quer chegar em metros: "))
+numded = altura/degrau
+print(f"A quantidade de degraus que você irá subir é: {round(numded,2)}")
